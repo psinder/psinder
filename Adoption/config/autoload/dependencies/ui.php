d069fbf9a54a591c3da5a3638aa571cec986027d
Mateusz Sip <mateusz.sip@gmail.com>
2020-03-20 22:38:45 +0100

    Fix tests

--- Adoption/config/autoload/dependencies/ui.php
+++ Adoption/config/autoload/dependencies/ui.php
@@ -3,7 +3,12 @@
 declare(strict_types=1);
 
 use Lcobucci\Clock\Clock;
+use Nyholm\Psr7\Factory\Psr17Factory;
 use Psr\Container\ContainerInterface;
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\ServerRequestFactoryInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\UriFactoryInterface;
 use Sip\Psinder\Adoption\UI\Http\Shelter\PostOfferRequestHandler;
 use Sip\Psinder\SharedKernel\Application\Command\CommandBus;
 use Sip\Psinder\SharedKernel\Infrastructure\Serializer\Serializer;
@@ -12,6 +17,7 @@ use Sip\Psinder\SharedKernel\UI\Http\Middleware\JsonDeserializeMiddleware;
 use Sip\Psinder\SharedKernel\UI\Http\Middleware\JsonDeserializeToSpecifiedDTOMiddlewareFactory;
 use Sip\Psinder\SharedKernel\UI\Http\Middleware\RequestPayloadTargetDTOResolver;
 use Sip\Psinder\SharedKernel\UI\Http\Middleware\RouteResultRequestPayloadTargetDTOResolver;
+use Sip\Psinder\SharedKernel\UI\Http\RequestBuilderFactory;
 
 return [
     'dependencies' => [
@@ -39,6 +45,14 @@ return [
             JsonDeserializeToSpecifiedDTOMiddlewareFactory::class => static function (ContainerInterface $c) {
                 return new JsonDeserializeToSpecifiedDTOMiddlewareFactory($c->get(Serializer::class),);
             },
+            RequestBuilderFactory::class => static function(ContainerInterface $container) {
+                return new RequestBuilderFactory(
+                    new Psr17Factory(),
+                    new Psr17Factory(),
+                    new Psr17Factory(),
+                    new Psr17Factory(),
+                );
+            }
         ],
     ],
 ];
---
+++ Adoption/test/UI/Http/Shelter/PostRegisterShelterRequestHandlerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sip\Psinder\Adoption\Test\UI\Http\Shelter;
+
+use Faker\Factory;
+use PHPUnit\Framework\Constraint\IsType;
+use Sip\Psinder\Adoption\Test\FunctionalTestCase;
+use Sip\Psinder\SharedKernel\UI\Http\RequestBuilderFactory;
+
+final class PostRegisterShelterRequestHandlerTest extends FunctionalTestCase
+{
+    public function testPostsValidOffer() : void
+    {
+        $faker   = Factory::create();
+        $request = $this->get(RequestBuilderFactory::class)
+            ->createServerRequestBuilder()
+            ->post()
+            ->url('/register/shelter')
+            ->jsonBodyArray([
+                'email' => $faker->email,
+                'password' => $faker->password,
+                'name' => $faker->name,
+                'addressStreet' => $faker->streetName,
+                'addressNumber' => $faker->buildingNumber,
+                'addressPostalCode' => $faker->postcode,
+                'addressCity' => $faker->city,
+            ])->create();
+
+        $response = $this->handle($request);
+
+        self::assertResponseIsSuccess($response);
+        self::assertMessageBodyMatchesJson($response, [
+            '$.id' => new IsType(IsType::TYPE_STRING),
+        ]);
+    }
+}
